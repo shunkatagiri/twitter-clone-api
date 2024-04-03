783e6a64db4434e2b704583ea52aba8fab4310fd
katagiri <shun.katagiri1214@gmail.com>
2024-04-03 23:21:42 +0900

    TweetsControllerにCRUDアクションを実装

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,2 +1,44 @@
 class Api::V1::TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :update, :destroy]
+
+  def index
+    tweets = Tweet.all
+    render json: tweets
+  end
+
+  def show
+    render json: @tweet
+  end
+
+  def create
+    tweet = Tweet.new(tweet_params)
+    if tweet.save
+      render json: tweet, status: :created
+    else
+      render json: tweet.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      render json: @tweet
+    else
+      render json: @tweet.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    head :no_content
+  end
+
+  private
+
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
+
+    def tweet_params
+      params.require(:tweet).permit(:content, :user_id)
+    end
 end
