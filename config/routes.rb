d05ec8c74d72302571afe4a8b286db9d1b205c1a
katagiri <shun.katagiri1214@gmail.com>
2024-04-28 19:10:47 +0900

    API v1 ネームスペースでのusersとtweetsのリソースルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-namespace :api do
-  namespace :v1 do
-    resources :users
-    resources :tweets
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :tweets
+    end
   end
+  mount_devise_token_auth_for 'User', at: 'auth'
 end
