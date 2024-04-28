6a511c9c00989410b09761c5f9be3b5d851bfa24
katagiri <shun.katagiri1214@gmail.com>
2024-04-28 19:06:51 +0900

    devise_token_authを用いたUserモデルのセットアップと認証機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  extend Devise::Models  # 追加
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
 end
