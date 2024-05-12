83ff88acd8bd7b4ae4ee469c025c4472073fe0b1
katagiri <shun.katagiri1214@gmail.com>
2024-05-12 19:11:59 +0900

    CORS設定を更新し、開発環境のドメインを許可し、公開するヘッダーを追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,9 @@
-# Be sure to restart your server when you modify this file.
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3001'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :patch, :put, :delete, :options]
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
